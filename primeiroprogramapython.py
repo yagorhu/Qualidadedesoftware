fcf08155a00e5e577f4fc46b923220be66d10308
Yago Rhuan <yagorhuan25@gmail.com>
2023-10-03 17:18:16 -0300

    Primeiro Commit

---
+++ primeiroprogramapython.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  5 15:48:45 2023
+
+@author: 55389
+"""
+
+a=float(input("Informe o valor de a: "))
+print("a = ",a)
+b=float(input("Informe o valor de b: "))
+print("b = ",b)
+# Soma
+soma=a+b
+# Mostra o resultado
+print("Total = ",soma)
